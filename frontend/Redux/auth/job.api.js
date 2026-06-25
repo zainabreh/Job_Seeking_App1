5e4a50fb1b8af243c9d5f626b454e0a441c87f29
Zainab <156816915+zainabreh@users.noreply.github.com>
2026-06-24 21:34:38 -0700

    Update baseUrl to use import.meta.env

--- frontend/Redux/auth/job.api.js
+++ frontend/Redux/auth/job.api.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const jobApi = createApi({
   reducerPath: "jobApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.VITE_BACKEND_URL,
+    baseUrl: import.meta.env.VITE_BACKEND_URL,
     credentials: "include",
     mode: "cors"
   }),
